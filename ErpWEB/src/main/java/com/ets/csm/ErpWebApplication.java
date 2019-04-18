0ef3610eafeae333ef6117a5ab02d73140d92ab9
Pradipto Roy <pradipto.roy@theetsindia.com>
2019-04-18 15:08:33 +0530

    update

---
+++ ErpWEB/src/main/java/com/ets/csm/ErpWebApplication.java
@@ -0,0 +1,13 @@
+package com.ets.csm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErpWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErpWebApplication.class, args);
+	}
+
+}
